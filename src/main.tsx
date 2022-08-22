4907fb51ea5da3773fd64d92e5526adb7df3fc75
Harman Goei <5001432+hrgui@users.noreply.github.com>
2022-08-21 23:12:25 -0700

    refactor: remove turborepo and just simplify to vite

---
+++ src/main.tsx
@@ -0,0 +1,4 @@
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<div>Hello World</div>);
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.ts", "./src/**/*.tsx", "./.storybook/*.js", "./.storybook/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
