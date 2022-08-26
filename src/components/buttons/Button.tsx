51c24b4b4e86d32b6aac971706b74971e35aabb4
Harman Goei <5001432+hrgui@users.noreply.github.com>
2022-08-25 21:35:54 -0700

    feat: button and button effect and colors - remove ring as it looked awkward

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -4,43 +4,61 @@ import { twMerge } from "tailwind-merge";
 import ctl from "@netlify/classnames-template-literals";
 
 const baseStyles = ctl(`
-    focus:ring-4 
     font-medium 
     rounded-full 
     text-sm 
     px-5 
     py-2.5 
-    focus:outline-none 
+    focus:outline-none
+    transition-all
+    relative
+    group
+    overflow-hidden
 `);
 
 const variantClassNameMap = {
   default: ctl(
     `text-white
-    focus:ring-blue-300 
-    dark:bg-blue-600 
-    dark:hover:bg-blue-700 
-    dark:focus:ring-blue-800`
+    bg-blue-600
+    hover:bg-blue-700
+    active:bg-blue-800
+
+    dark:bg-blue-500 
+    dark:hover:bg-blue-600
+    dark:active:bg-blue-700
+    `
   ),
   danger: ctl(
     `text-white
-    focus:ring-red-300 
-    dark:bg-red-600 
-    dark:hover:bg-red-700 
-    dark:focus:ring-red-800`
+    bg-red-600
+    hover:bg-red-700
+    active:bg-red-800
+
+    dark:bg-red-500 
+    dark:hover:bg-red-600
+    dark:active:bg-red-700
+    `
   ),
   success: ctl(
     `text-white
-    focus:ring-green-300 
-    dark:bg-green-600 
-    dark:hover:bg-green-700 
-    dark:focus:ring-green-800`
+    bg-green-600
+    hover:bg-green-700
+    active:bg-green-800
+
+    dark:bg-green-500 
+    dark:hover:bg-green-600
+    dark:active:bg-green-700`
   ),
   warning: ctl(
-    `text-white
-    focus:ring-yellow-300 
-    dark:bg-yellow-600 
-    dark:hover:bg-yellow-700 
-    dark:focus:ring-yellow-800`
+    `text-white 
+    bg-yellow-600
+    hover:bg-yellow-700
+    active:bg-yellow-800
+
+
+    dark:bg-yellow-500 
+    dark:hover:bg-yellow-600
+    dark:active:bg-yellow-700`
   ),
 };
 
@@ -54,6 +72,7 @@ type Props = {
 export const Button = ({
   variant = "default",
   className: inputtedClassName,
+  children,
   ...props
 }: Props) => {
   function getVariantClassName(variant: Props["variant"]) {
@@ -63,7 +82,12 @@ export const Button = ({
   const className = twMerge(
     cx(baseStyles, getVariantClassName(variant), inputtedClassName)
   );
-  return <button className={className} {...props} />;
+  return (
+    <button className={className} {...props}>
+      <span className="absolute w-0 h-0 transition-all duration-300 ease-out bg-white rounded-full group-hover:w-full group-hover:h-full top-0 left-0 opacity-10"></span>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
