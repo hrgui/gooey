3e2b230db89861cf6e6ec97d6ae7b709815dc023
Harman Goei <5001432+hrgui@users.noreply.github.com>
2022-08-27 18:16:57 -0700

    feat(Input): expose input family

---
+++ src/components/input/index.ts
@@ -0,0 +1,4 @@
+export * from "./Input";
+export * from "./switch";
+export * from "./textarea";
+export * from "./select";
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export * from "./components/input/switch";
 export * from "./components/tabs";
 export * from "./components/buttons";
 export * from "./components/toasts";
 export * from "./components/drawer";
 export * from "./components/overlay";
+export * from "./components/input";
